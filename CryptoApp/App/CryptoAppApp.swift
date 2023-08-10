93087dc55052110e8bf3dd3c3e59c8011d3fe9d2
ivan <tellezivan7@gmail.com>
2023-08-09 20:43:10 -0700

    feat: setup initial app directory

---
+++ CryptoApp/App/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Telles Ivan on 8/9/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+      		 HomeView()
+        }
+    }
+}
---
+++ CryptoApp/Core/Home/Views/HomeView.swift
@@ -0,0 +1,26 @@
+//
+//  HomeView.swift
+//  CryptoApp
+//
+//  Created by Telles Ivan on 8/9/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+       NavigationStack {
+          ScrollView(.vertical, showsIndicators:false) {
+//             top movers view
+             
+//             aLL coins view
+          }
+          .navigationTitle("Live Prices")
+          
+       }
+    }
+}
+
+#Preview {
+    HomeView()
+}
